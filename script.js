14261e250cc638fc7a33b33c6eac86fe7fa2467b
tommasoc <tommasocantarini4@gmail.com>
2019-03-07 17:14:00 +0100

    fine

---
+++ script.js
@@ -0,0 +1,154 @@
+
+function random(min, max){
+  return Math.floor(Math.random()*(max - min)) + min;
+}
+
+function randomChar(){
+  var rndInd = random(65,90);
+  var rndChar = String.fromCharCode(rndInd);
+
+  return rndChar;
+}
+
+function idRandom(){
+
+  var idRnd = "";
+  var num = "";
+  var char = "";
+
+  for (var i = 0; i < 3; i++) {
+
+    num += random(0,9);
+    char += randomChar();
+  }
+
+  idRnd = char + num;
+
+  return idRnd;
+}
+
+function randomPlayer(){
+
+  var twoPerc = random(0,99);
+  var threePerc = 100 - twoPerc;
+
+  var player = {
+    "id": idRandom() ,
+    "points": random(0,100) ,
+    "bounce": random(0,500),
+    "mistake": random(0,50),
+    "twoPerc": twoPerc,
+    "threePerc": threePerc,
+  };
+
+  return player;
+}
+
+function isPresent(player, players){
+
+  var finded = false;
+  for (var i = 0; i < players.length; i++) {
+    if (player.id == players[i].id) {
+      finded = true;
+    }
+  }
+  return finded;
+}
+
+function getIdPlayer(id, players){
+
+  var player ;
+
+  for (var i = 0; i < players.length; i++) {
+
+    if (players[i].id == id) {
+
+      player = players[i];
+    }
+  }
+  return player;
+}
+
+function getRandomPlayers(){
+  var players = [];
+
+  while (players.length < 10) {
+
+    var player = randomPlayer();
+
+    if (!isPresent(player, players)) {
+
+      players.push(player);
+    }
+  }
+  return players;
+}
+
+function upDateList(players){
+
+  for (var i = 0; i < players.length; i++) {
+    var player = players[i];
+    var option = document.createElement("option");
+    option.value = player.id;
+
+    var datalist = $("datalist");
+    datalist.append(option);
+  }
+}
+
+function clearClick(){
+  var input = $("#usr-input");
+  input.val("");
+
+  var  idDOM = $("#id > span.content");
+  var  pointsDOM = $("#points > span.content");
+  var  bounceDOM = $("#bounce > span.content");
+  var  mistakeDOM = $("#mistake > span.content");
+  var  twoPercDOM = $("#twoPerc > span.content");
+  var  threePercDOM = $("#threePerc > span.content");
+
+  idDOM.text("");
+  pointsDOM.text("");
+  bounceDOM.text("");
+  mistakeDOM.text("");
+  twoPercDOM.text("");
+  threePercDOM.text("");
+}
+
+function upCompleteId(players){
+
+  var me = $("#usr-input");
+  var selectId = me.val();
+
+  var player = getIdPlayer(selectId, players);
+
+  var  idDOM = $("#id > span.content");
+  var  pointsDOM = $("#points > span.content");
+  var  bounceDOM = $("#bounce > span.content");
+  var  mistakeDOM = $("#mistake > span.content");
+  var  twoPercDOM = $("#twoPerc > span.content");
+  var  threePercDOM = $("#threePerc > span.content");
+
+  idDOM.text(player.id);
+  pointsDOM.text(player.points);
+  bounceDOM.text(player.bounce);
+  mistakeDOM.text(player.mistake);
+  twoPercDOM.text(player.twoPerc + "%");
+  threePercDOM.text(player.threePerc + "%");
+}
+
+
+function init() {
+  var players = getRandomPlayers();
+  upDateList(players);
+
+  var btn = $("#clear-btn");
+  btn.click(clearClick);
+
+  var idPlayer = $("#usr-input");
+  idPlayer.on("change" , function(){
+    upCompleteId(players);
+  });
+}
+
+$(document).ready(init);
